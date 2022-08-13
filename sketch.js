d686a91b967405ef7675f563404cbee7a646012a
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2022-08-13 16:42:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
-var starImg,bgImg;
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
 var star, starBody;
-//criar variável para sprite de fada e imgFada
-var fada, fadaImg;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -10,27 +9,33 @@ const Body = Matter.Body;
 
 function preload()
 {
-    starImg = loadImage("images/star.png");
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
-    //carregar animação de fada 
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
 
-    //carregar som vozFada
-    
 }
 
 function setup() {
-    createCanvas(800, 750);
+	createCanvas(800, 750);
 
-    //escrever código para tocar o som vozFada
+	fairyVoice.play();
 
-    //criar sprite de fada e adicionar animação para fada
-   
+	fairy = createSprite(130, 520);
+	//adicionar animção à fada
+	//fairy.setAnimation("fairyflying",fairyImg); 
+	//fairy.addAnimation("fairyflying");  
+	//fairy.addAnimation("fairyflying",fairyImg);  
+	//fairy.addAnimation(fairyImg);  
+	
+	fairy.scale =0.25;
 
-    star = createSprite(650,30);
+	star = createSprite(650,30);
 	star.addImage(starImg);
 	star.scale = 0.2;
 
-    engine = Engine.create();
+
+	engine = Engine.create();
 	world = engine.world;
 
 	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
@@ -40,23 +45,42 @@ function setup() {
 
 }
 
-function draw(){
-    background(bgImg);
 
-    star.y = starBody.position.y;
+function draw() {
+  background(bgImg);
 
-    //fazer a estrela parar na altura da mão da fada
-    
-    if(keyIsDown(DOWN_ARROW)){
-        Matter.Body.setStatic(starBody, false);
-    }
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
 
-    if(keyDown("left")){
-        fada.x -= 5;
-    }
-    if(keyDown("right")){
-        fada.x += 5;
-    }
+  console.log(star.y);
 
-    drawSprites();
+  if(star.y > 470 && starBody.position.y > 470 ){
+  	Matter.Body.setStatic(starBody,true);
+  }
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if(keyCode === RIGHT_ARROW){
+		//mover fada para direita
+           	//fairy = fairy.x + 20;
+		//fairy.y = fairy.x + 20;
+		//fairy.x =  20;
+		//fairy.x = fairy.x + 20;
+	}
+	
+        if(keyCode === LEFT_ARROW){
+           fairy.x = fairy.x - 20;
+	}
+
+	if (keyCode === DOWN_ARROW) {
+		//codigo para deixar a estrela estática ao parar na mão da fada
+		//Body.setStatic(starBody,false); 
+		//Matter.Body.setStatic(false); 
+		//Body.setStatic(starBody,false); 
+		//Matter.Body.setStatic(starBody,true); 
+	}
 }
